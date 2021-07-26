3eb7f9f3983d625a669a01450f00963b3da08f79
Sarah Helgen <sarah.helgen@gmail.com>
2021-07-26 18:30:07 -0500

    fixed mistake on line 18

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -15,7 +15,7 @@ let supplyChanges = [3, 5, -6, 0, 7, 11 ];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
-console.log( supplyChanges );
+console.log( supplyChanges[1] );
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
